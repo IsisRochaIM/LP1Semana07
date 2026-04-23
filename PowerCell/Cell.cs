bbe19305e43d8ba24a6bf3958c8b800469eb205a
Isis <mejaikatwt@gmail.com>
2026-04-23 16:27:34 +0100

    passo 4 e parte do 5

--- PowerCell/Cell.cs
+++ PowerCell/Cell.cs
@@ -7,6 +7,69 @@ namespace PowerCell
 {
     public class Cell
     {
+        private float charge;
         
+        public string Name { get; }
+        public float Charge
+        {
+            get
+            {
+                return charge;
+            }
+
+            set
+            {
+                if (value > 200)
+                {
+                    charge = 200;
+                }
+                else if( value < 0)
+                {
+                    charge = 0;
+                }
+                else
+                {
+                    charge = value;
+                }
+
+            }
+        }
+
+        public int Level
+        {
+            get
+            {
+                int val = 1 + Convert.ToInt32(Charge)/40;
+                return val;
+            }
+        }
+
+        public void Consume(float amount)
+        {
+            if (amount < Charge)
+            {
+                Charge -= amount;
+            }
+            else
+            {
+                Charge = 0;
+            }
+        }
+
+        public void Restore()
+        {
+            Charge = 200;
+        }
+
+        public Cell (string name)
+        {
+            Name = name;
+            Charge = 200;
+        }
+
+        public override string ToString()
+        {
+            return $"[{Name}] Level {Level}: {Charge:F0}/200";
+        }
     }
 }
